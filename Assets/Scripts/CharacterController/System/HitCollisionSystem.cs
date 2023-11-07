4ee0f5fbc9f3f08615b20eba469c1b095a62a32f
DOKUNYU <104719627+DOKUNYU@users.noreply.github.com>
2023-11-07 21:36:59 +0800

    子弹碰撞逻辑更改

--- Assets/Scripts/CharacterController/System/HitCollisionSystem.cs
+++ Assets/Scripts/CharacterController/System/HitCollisionSystem.cs
@@ -23,6 +23,7 @@ namespace CharacterController
             
             state.RequireForUpdate<ShootSetting>();
             state.RequireForUpdate(bloodControllerQuery);
+            state.RequireForUpdate<Bullet>();
             state.RequireForUpdate<Harm>();
             state.RequireForUpdate<SimulationSingleton>();
         }
@@ -34,6 +35,7 @@ namespace CharacterController
             {
                 harmData = SystemAPI.GetComponentLookup<Harm>(),
                 blood = SystemAPI.GetComponentLookup<BloodComponent>(),
+                bullets = SystemAPI.GetComponentLookup<Bullet>()
             }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
         }
         
@@ -42,6 +44,7 @@ namespace CharacterController
         {
             [ReadOnly] public ComponentLookup<Harm> harmData;
             public ComponentLookup<BloodComponent> blood;
+            public ComponentLookup<Bullet> bullets;
 
             public void Execute(CollisionEvent collisionEvent)
             {
@@ -49,25 +52,44 @@ namespace CharacterController
                 Entity entityB = collisionEvent.EntityB;
 
                 bool isABullet = harmData.HasComponent(entityA);
-
-                Debug.Log("击中！！");
+                bool isBBullet = harmData.HasComponent(entityB);
+                
 
                 //A是子弹
                 if (isABullet)
                 {
-                    var damage = harmData[entityA].injury;
-                    var victimBlood = blood[entityB];
-                    victimBlood.hp-=damage;
-                    blood[entityB] = victimBlood;
+                    var bulletData = bullets[entityA];
+                    if (!bulletData.IsCollision)
+                    {
+                        //扣血
+                        var damage = harmData[entityA].injury;
+                        var victimBlood = blood[entityB];
+                        victimBlood.hp-=damage;
+                        blood[entityB] = victimBlood;
+                        //状态改变
+                        bulletData.IsCollision = true;
+                        bullets[entityA] = bulletData;
+                        Debug.Log("击中！！ :"+entityB.Index);
+                    }
                 }
                 //B是子弹
-                else
+                else if(isBBullet)
                 {
-                    var damage = harmData[entityB].injury;
-                    var victimBlood = blood[entityA];
-                    victimBlood.hp-=damage;
-                    blood[entityA] = victimBlood;
+                    var bulletData = bullets[entityB];
+                    if (!bulletData.IsCollision)
+                    {
+                        //扣血
+                        var damage = harmData[entityB].injury;
+                        var victimBlood = blood[entityA];
+                        victimBlood.hp -= damage;
+                        blood[entityA] = victimBlood;
+                        //状态改变
+                        bulletData.IsCollision = true;
+                        bullets[entityB] = bulletData;
+                        Debug.Log("击中！！ :"+entityA.Index);
+                    }
                 }
+                //其他情况不是子弹撞击
                 
             }
         }
